ef5824c1cf7b9c792ea1fd95923f5eb160ca0955
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2024-07-23 10:34:56 +0300

    1.3

--- main/views.py
+++ main/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+def personal(request):
+    return render(request, 'personal.html')
+def job(request):
+    return render(request, 'job.html')
+def place(request):
+    return render(request, "place.html")
+def partner(request):
+    return render(request, 'partner.html')
+def contact(request):
+    return render(request, 'contact.html')
