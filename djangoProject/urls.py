ce2a46bdb82df91ea8eec28bf4ea1e30a312eede
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2024-07-24 10:56:45 +0300

    126.345

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from djangoProject import settings
 from main import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='index'),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('partner', views.partner, name='partner'),
     path('contact', views.contact, name='contact'),
     path('admin/', admin.site.urls, name='admin'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ObjectMain
 
-# Register your models here.
+@admin.register(ObjectMain)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image')
